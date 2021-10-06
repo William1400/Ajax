c94e25d7a7ca2f50eb97bd9b41405f26adea8646
William.B <william.bady.1400@gmail.com>
2021-10-06 14:14:20 +0200

    finish

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const img = document.getElementById('img');
+const author = document.getElementById('author');
+const quote = document.getElementById('quote');
+
+//const totalQuotes = document.childNodes('total_quotes');
+
+fetch("https://thatsthespir.it/api")
+
+    .then(res => {
+
+        if (res.ok) {
+
+            res.json().then(data => {
+                author.innerHTML = data.author
+                img.src = data.photo
+                quote.innerHTML = data.quote
+
+            })
+
+        } else {
+
+            console.log("error");
+            document.getElementById('error').innerHTML = "Fatal error";
+        }
+        
+    });
+
+
+
+
+
 
